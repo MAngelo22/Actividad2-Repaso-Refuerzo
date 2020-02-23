4b32b689c4085b7ce9bad2b04bfc975df3bb2f9d
Miguel <miguelangelnunezlopez96@gmail.com>
2020-02-23 20:14:07 +0100

    Primer Commit

---
+++ Actividad 2 Repaso/src/Usuario.java
@@ -0,0 +1,40 @@
+
+public class Usuario {
+
+	public static void main(String[] args) {
+		
+		
+		//--------Menu--------
+		System.out.println(
+				""+
+				"1- Alta de pelicula\r\n" + 
+				"2- Listar Pelicula\r\n" + 
+				"3- Buscar Pelicula por id\r\n" + 
+				"4- Buscar pelicula por titulo\r\n" + 
+				"5- Buscar Peliculas por genero\r\n" + 
+				"6- Borrar pelicula por id\r\n" + 
+				"7- Salir de la aplicación");
+		
+		
+		
+		
+		
+		
+		
+		//---------------------
+		Peliculas Peli1;
+	
+		Peli1 = new Peliculas("P01", "BlackHat, Amenaza en la red", "Michael Mann", "Thriller, Acción|Crimen. Espionaje. Internet|Informática", 2015);
+		
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
