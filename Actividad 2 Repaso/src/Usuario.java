cb11dbe0173fcd27f81e34f2134b89cba099a468
Miguel <miguelangelnunezlopez96@gmail.com>
2020-02-24 00:09:11 +0100

    Añadido Switch case

--- Actividad 2 Repaso/src/Usuario.java
+++ Actividad 2 Repaso/src/Usuario.java
@@ -1,12 +1,12 @@
+import java.util.Scanner;
 
 public class Usuario {
 
 	public static void main(String[] args) {
 		
-		
 		//--------Menu--------
 		System.out.println(
-				""+
+				"Selecciona :\r\n"+
 				"1- Alta de pelicula\r\n" + 
 				"2- Listar Pelicula\r\n" + 
 				"3- Buscar Pelicula por id\r\n" + 
@@ -14,23 +14,42 @@ public class Usuario {
 				"5- Buscar Peliculas por genero\r\n" + 
 				"6- Borrar pelicula por id\r\n" + 
 				"7- Salir de la aplicación");
-		
-		
-		
-		
-		
-		
-		
-		//---------------------
-		Peliculas Peli1;
+		//-------Crear-Peliculas------
+		Peliculas Peli1,Peli2,Peli3;
 	
-		Peli1 = new Peliculas("P01", "BlackHat, Amenaza en la red", "Michael Mann", "Thriller, Acción|Crimen. Espionaje. Internet|Informática", 2015);
-		
-		
+		Peli1 = new Peliculas("P1", "BlackHat, Amenaza en la red", "Michael Mann", "Thriller, Acción|Crimen", 2015);
+		Peli1 = new Peliculas("P2", "The Imitation Game(Descifrando Enigma)","Morten Tyldum", "Thriller, Drama|Biográfico", 2014);
+		Peli1 = new Peliculas("P3", "Avengers Endgame", "Anthony Russo, Joe Russo (Hrmns Russo)", "Ciencia Ficción, Acción|Superheroes", 2019);
 	
+		//-----Interaccion-Usuario-----
+		Scanner teclado = new Scanner(System.in);
+		int Boton = teclado.nextInt();
 	
-	
-	
+		//-----Funciones-Botones-------
+		String Texto = null;
+		switch (Boton) {
+			case 1:
+				Texto = "Has pulsado 1";
+				break;
+			case 2:
+				Texto = "Has pulsado 2";
+				break;
+			case 3:
+				Texto = "Has pulsado 3";
+				break;
+			case 4:
+				Texto = "Has pulsado 4";
+				break;
+			case 5:
+				Texto = "Has pulsado 5";
+				break;
+			case 6:
+				Texto = "Has pulsado 6";
+				break;
+			case 7:
+				Texto = "Has pulsado 7";
+				break;
+		}System.out.println(Texto);
 	
 	
 	
